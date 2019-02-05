bc7a20bb561645edec3849a64ed0c46b5da2bb8e
Robin Hekmatara <robinhekmatara@gmail.com>
2019-02-05 20:37:58 +0100

    Created Population component, unstyled

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom'
 import {BY_CITY, BY_COUNTRY} from './Strings';
 import './App.css';
 import SearchCity from './Components/SearchCity';
+import Population from './Components/Population';
 
 class App extends Component {
   render() {
@@ -16,7 +17,7 @@ class App extends Component {
           <Switch>
             <Route path="/search_by_city" component={SearchCity}/>
             <Route path="/search_by_country"/>
-            <Route path="/population/:city"/>
+            <Route path="/population/:city" render={props => <Population {...props}/>}/>
             <Route path="/" render={() => 
               <div>
                 <Link to='/search_by_city'>{BY_CITY}</Link>
---
+++ src/Components/Population.jsx
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+
+class Population extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {location, city} = this.props;
+    const population = location.state.population;
+    return (
+      <div>
+        <h2>{city}</h2>
+        <p>POPULATION</p>
+        <p>{population}</p>
+      </div>
+    )
+  }
+}
+
+
+export default Population;
