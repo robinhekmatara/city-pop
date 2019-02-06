cb510ff2e0825e16328d50231d7f946fda7c6683
Robin Hekmatara <robinhekmatara@gmail.com>
2019-02-06 22:36:44 +0100

    Moved numToPaddedString to stringOperations and renamed to spaceEveryThree

--- src/Components/Population.jsx
+++ src/Components/Population.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { capitalizeSentence } from '../Utils/stringOperations'
+import { capitalizeSentence, spaceEveryThree } from '../Utils/stringOperations'
 import '../styles/population.css';
 
 const Population = ({location, match}) =>
@@ -9,23 +9,8 @@ const Population = ({location, match}) =>
     </header>
     <div id="population">
       <p>POPULATION</p>
-      <p id="population-text">{numToPaddedStr(location.state.population)}</p>
+      <p id="population-text">{spaceEveryThree(String(location.state.population))}</p>
     </div>
   </div>
-
-const numToPaddedStr = (num) => {
-  let str = String(num);
-  let len = str.length;
-  let first = len % 3;
-
-  let newStr = str.substring(0, first);
-  console.log(newStr);
-  for (let i = first; i < str.length; i+=3){
-    newStr += ' ';
-    newStr += str.substring(i, i + 3);
-  }
-
-  return newStr;
-}
 
 export default Population;
--- src/Utils/stringOperations.js
+++ src/Utils/stringOperations.js
@@ -1,1 +1,19 @@
 export const capitalizeSentence = str => str.charAt(0).toUpperCase() + str.slice(1);
+
+
+/*
+  Insert space every three letter starting from end of string
+*/
+export const spaceEveryThree = (str) => {
+  let len = str.length;
+  let first = len % 3;
+
+  let newStr = str.substring(0, first);
+  console.log(newStr);
+  for (let i = first; i < str.length; i+=3){
+    newStr += ' ';
+    newStr += str.substring(i, i + 3);
+  }
+
+  return newStr;
+}
