f3504f0b19aac9e0d8934356d7ecfe9fa405b023
Robin Hekmatara <robinhekmatara@gmail.com>
2019-02-06 14:59:42 +0100

    Refactored header in app.js to its own component

--- src/App.js
+++ src/App.js
@@ -6,28 +6,28 @@ import SearchCity from './Containers/SearchCity';
 import SearchCountry from './Containers/SearchCountry';
 import Population from './Components/Population';
 import List from './Components/List'
+import Header from './Components/Header'
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <header>
-            <h1>CityPop</h1>
-          </header>
-
-          <Switch>
-            <Route path="/search_by_city" component={SearchCity}/>
-            <Route path="/search_by_country" component={SearchCountry}/>
-            <Route path="/population/:city" render={props => <Population {...props}/>}/>
-            <Route path="/country/:country" render={props => <List {...props}/>}/>
-            <Route path="/" render={() => 
-              <div>
-                <Link to='/search_by_city'>{BY_CITY}</Link>
-                <Link to='/search_by_country'>{BY_COUNTRY}</Link>
-              </div>
-            }/>
-          </Switch>
+          <Header>CityPop</Header>
+          <main>
+            <Switch>
+              <Route path="/search_by_city" component={SearchCity}/>
+              <Route path="/search_by_country" component={SearchCountry}/>
+              <Route path="/population/:city" render={props => <Population {...props}/>}/>
+              <Route path="/country/:country" render={props => <List {...props}/>}/>
+              <Route path="/" render={() => 
+                <nav>
+                  <Link to='/search_by_city'>{BY_CITY}</Link>
+                  <Link to='/search_by_country'>{BY_COUNTRY}</Link>
+                </nav>
+              }/>
+            </Switch>
+          </main>
         </div>
       </Router>
     );
---
+++ src/Components/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = ({children}) =>
+  <header>
+    <h1>{children}</h1>
+  </header>
+
+export default Header;
