5da85617d2a37eb185b04ccfb5a872658d581cd4
Robin Hekmatara <robinhekmatara@gmail.com>
2019-02-06 20:56:24 +0100

    Header links to homepage

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/header.css';
 
 const Header = ({children}) =>
   <header id="page-header">
-    <h1>{children}</h1>
+    <h1>
+      <Link to="/">{children}</Link>
+    </h1>
   </header>
 
 export default Header;
